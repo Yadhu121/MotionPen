7ff63fe65c1954f37603ba646d3047618b235ea0
Yadhu121 <ryadhukrishnan0@gmail.com>
2026-01-08 15:28:48 +0530

    Lines are smoothened

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ mp_hands = mp.solutions.hands
 mp_draw = mp.solutions.drawing_utils
 
 prev_x, prev_y = None, None
+smooth_x, smooth_y = None, None
+alpha = 0.7
 canvas = None
 
 hands = mp_hands.Hands (
     static_image_mode = False,
     max_num_hands = 1,
     min_detection_confidence = 0.7,
-    min_tracking_confidence = 0.7
+    min_tracking_confidence = 0.8
 )
 
 cap = cv2.VideoCapture(1)
@@ -32,6 +34,10 @@ while True:
         for hand_landmarks in result.multi_hand_landmarks:
             mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
+            if canvas is None:
+                canvas = frame.copy()
+                canvas[:] = 0
+            
             h, w, _ = frame.shape
 
             lm = hand_landmarks.landmark[8]
@@ -39,17 +45,19 @@ while True:
             x = int(lm.x * w)
             y = int(lm.y * h)
 
-            if canvas is None:
-                canvas = frame.copy()
-                canvas[:] = 0
+            if smooth_x is None:
+                smooth_x, smooth_y = x, y
+            else:
+                smooth_x = int(alpha * smooth_x + (1 - alpha) * x)
+                smooth_y = int(alpha * smooth_y + (1 - alpha) * y)
 
             if prev_x is not None and prev_y is not None:
-                cv2.line(canvas, (prev_x, prev_y), (x, y), (255, 0, 0), 5)
+                cv2.line(canvas, (prev_x, prev_y), (smooth_x, smooth_y), (255, 0, 0), 8)
 
             frame = cv2.add(canvas, frame)
-            prev_x, prev_y = x, y
+            prev_x, prev_y = smooth_x, smooth_y
 
-            cv2.circle(frame, (x,y), 10, (0, 0, 255), -1)
+            cv2.circle(frame, (smooth_x,smooth_y), 10, (0, 0, 255), -1)
 
     else:
         prev_x, prev_y = None, None
