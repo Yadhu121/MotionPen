ec7245eb441a397e79dc82defbf7ef24b8058587
Yadhu121 <ryadhukrishnan0@gmail.com>
2026-01-08 15:10:56 +0530

    Detects hand and marks landmarks and connections

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+mp_draw = mp.solutions.drawing_utils
+
+hands = mp_hands.Hands (
+    static_image_mode = False,
+    max_num_hands = 1,
+    min_detection_confidence = 0.7,
+    min_tracking_confidence = 0.7
+)
 
 cap = cv2.VideoCapture(1)
 
@@ -10,6 +21,14 @@ while True:
         break
 
     frame = cv2.flip(frame, 1)
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    result = hands.process(rgb)
+
+    if result.multi_hand_landmarks:
+        for hand_landmarks in result.multi_hand_landmarks:
+            mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
     cv2.imshow("Camera", frame)
 
     if cv2.waitKey(1) & 0xFF == 27:
