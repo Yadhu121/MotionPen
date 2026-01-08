47c889beac1c42f9ffea26f671cde8bc7f196176
Yadhu121 <ryadhukrishnan0@gmail.com>
2026-01-08 14:58:30 +0530

    basic openCV flipped live video

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2
+
+cap = cv2.VideoCapture(1)
+
+while True:
+
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    frame = cv2.flip(frame, 1)
+    cv2.imshow("Camera", frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
